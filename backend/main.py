e6ca45fbfe44dcc9b6f42e599c94b1fe5c1e6a56
ElectraZarafeta <e.zarafeta@hotmail.com>
2023-02-07 21:34:05 +0100

    Insert and delete task

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,29 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 DUMMY_TASK = {
-    "1":
+    1:
     {    "id": 1,
         "title": "skoupisma",
-        "category-id": 1,
-        "asignee-id": 1,
-        "users-id": [1],
-        "group-id": 1},
-    "2":
+        "category_id": 1,
+        "asignee_id": 1,
+        "users_id": [1],
+        "group_id": 1},
+    2:
     {    "id": 2,
         "title": "skoupisma",
-        "category-id": 1,
-        "asignee-id": 1,
-        "users-id": [1, 2],
-        "group-id": 1},
-    "3":
+        "category_id": 1,
+        "asignee_id": 1,
+        "users_id": [1, 2],
+        "group_id": 1},
+    3:
     {    "id": 3,
         "title": "skoupisma",
-        "category-id": 1,
-        "users-id": [2],
-        "asignee-id": 1,
-        "group-id": 2},
+        "category_id": 1,
+        "users_id": [2],
+        "asignee_id": 1,
+        "group_id": 2},
 }
 
 DUMMY_CAT = {
@@ -34,19 +35,27 @@ DUMMY_CAT = {
 DUMMY_USER = {
     "id": 1,
     "name": "Mike",
-    "group-id": 1
+    "group_id": 1
 }
 
 DUMMY_GROUP = {
     "id": 1,
     "name": "mike and electra",
-    "users-id": [1, 2]
+    "users_id": [1, 2]
 }
 
-def find_tasks(val, dicts):
+class Task(BaseModel):
+    id: int
+    title: str
+    category_id: int
+    asignee_id: int
+    users_id: list
+    group_id: int
+
+def find_tasks(user_id, tasks):
     tmp = []
-    for v in dicts.values():
-        if val in v['users-id']:
+    for v in tasks.values():
+        if user_id in v['users_id']:
             tmp.append(v)
 
     if len(tmp) > 0:
@@ -73,6 +82,18 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{user_id}")
-def list(user_id: int):
+def list_tasks(user_id: int):
     
     return find_tasks(user_id, DUMMY_TASK)
+
+@app.post("/items/{user_id}")
+def insert(user_id: int, task: Task):
+    new_task_key = len(DUMMY_TASK) + 1
+    DUMMY_TASK[new_task_key] = task.dict()
+    
+    return DUMMY_TASK
+
+@app.delete("/items/{user_id}")
+def delete(user_id: int, task_id: int):
+    DUMMY_TASK.pop(task_id)
+    return DUMMY_TASK
